4dcb915afd48e16195391f738dc31c00e2b84068
sivam777 <sivam.evt@gmail.com>
2016-03-03 22:39:02 +0200

    Added files via upload

---
+++ 5.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main ()
+{
+	int a, os;
+	
+	printf("\n\nVvedite vozrast: ");
+	scanf("%d", &a);
+	os = a%10;
+	if (os == 1) {
+		printf("\nVam %d god.", a);
+	} else {
+		if ((os > 1)&&(os < 5)) {
+			printf ("\nVam %d goda.", a);
+		} else {
+			printf("\nVam %d let.", a);
+		}
+	}
+	
+	return 0;	
+}
