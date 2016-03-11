44c0345197ca97f0ae2d81c54d7cdbf986030c13
sivam777 <sivam.evt@gmail.com>
2016-03-11 08:20:53 +0200

    Update 7.c

--- 7.c
+++ 7.c
@@ -2,14 +2,14 @@
 
 int main ()
 {
-	int a, x, x2, x3;
+	int year, x, x2, x3;
 	
 	printf("\nVvedite god: ");
 	scanf("%d", &a);
 	
-	x = a%4;
-	x2 = a%100;
-	x3 = a%400;
+	x = year%4;
+	x2 = year%100;
+	x3 = year%400;
 	
 	if ((x2 == 0)&&(x3 != 0)) {
 		printf("\nV etom godu 365 dney.\n");
